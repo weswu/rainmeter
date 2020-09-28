0dcc6a86a698ff94bc095412829d2f4a04c86d88
weswu <836790625@qq.com>
2020-09-28 11:08:43 +0800

    wxw

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# rainmeter
+# rainmeter皮肤
+
+#### 本地目录
+`C:\Users\Administrator\Documents\Rainmeter\Skins`
+
+#### 添加国内music插件
+1. Variables.inc
+``` javascript;
+Player=KgMusic
+PlayerPath=C:\Program Files (x86)\KuGou\KGMusic\KuGou.exe     
+```
+
+2. Player.ini
+Plugin=Musicplayer.dll
+
+
+- NORTHWEST
+- Simple Clean
---
+++ Simple Clean XX/倒计时/CountUpSecond.lua
@@ -0,0 +1,37 @@
+
+PROPERTIES = {year=0, month=0, day=0, hour=0, min=0, sec=0, fintext=""}
+
+function Initialize()
+
+	RELEASEDATE = {}
+	setmetatable(RELEASEDATE, getmetatable(PROPERTIES))
+	for k,v in pairs(PROPERTIES) do
+		if k ~= fintext then
+			RELEASEDATE[k] = v
+		end
+	end
+	RELEASEDATE.isdst = true
+
+	RELEASETEXT = PROPERTIES.fintext or ""
+
+end
+
+function GetTimeLeft()
+	local dif = os.time() - os.time(RELEASEDATE)
+	local timeleft = {
+		[1] = math.floor(dif/60/60/24),	
+		[2] = math.floor(dif/60/60)%24,	
+		[3] = math.floor(dif/60)%60,	
+		[4] = math.floor(dif)%60		
+	}
+	return tostring(timeleft[4])
+end
+
+function Update()
+end
+
+function GetStringValue()
+
+	return GetTimeLeft()
+
+end
